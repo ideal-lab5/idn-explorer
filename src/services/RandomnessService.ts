2956b17a28ce5860a0e4fb2d5a5aa3226107ab78
Carlos Montoya <carloskiron@Carloss-MacBook-Pro.local>
2025-08-08 19:08:01 +0200

    fix-unrequired console logs and logic

--- src/services/RandomnessService.ts
+++ src/services/RandomnessService.ts
@@ -1,6 +1,5 @@
-// Copyright 2023-2024 Ideal Labs.
+// Copyright 2023-2025 Ideal Labs.
 // SPDX-License-Identifier: Apache-2.0
-
 import { ApiPromise } from '@polkadot/api';
 import type { Vec } from '@polkadot/types';
 import type { EventRecord } from '@polkadot/types/interfaces';
